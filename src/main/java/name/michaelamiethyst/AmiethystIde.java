8c93073633d54c5a99e68c7d57e52686ae6f3439
Michael Peter Gower Amiethyst <designatevoid@gmail.com>
2018-08-04 11:58:35 -0700

    Added comments to break up blocks of text.

--- src/main/java/name/michaelamiethyst/AmiethystIde.java
+++ src/main/java/name/michaelamiethyst/AmiethystIde.java
@@ -22,35 +22,53 @@ public class AmiethystIde extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drawing Operations Test");
-        Group root = new Group();
+        // prepare canvas
         Canvas canvas = new Canvas(300, 250);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         drawShapes(gc);
+
+        // put canvas into group
+        Group root = new Group();
         root.getChildren().add(canvas);
+
+        // prepare stage
+        primaryStage.setTitle("Drawing Operations Test");
+        // wrap Group root into a scene and add to stage
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
     private void drawShapes(GraphicsContext gc) {
+        // prep palette
         gc.setFill(Color.GREEN);
         gc.setStroke(Color.BLUE);
+
+        // draw line as a slash '/'
         gc.setLineWidth(5);
         gc.strokeLine(40, 10, 10, 40);
+
+        // draw circles
         gc.fillOval(10, 60, 30, 30);
         gc.strokeOval(60, 60, 30, 30);
+
+        // draw rectangles with rounded corners -- very snazzy
         gc.fillRoundRect(110, 60, 30, 30, 10, 10);
         gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+
+        // draw two rows of pac-mans
         gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
         gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
         gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
         gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
         gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
         gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+
+        // draw two hour-glass shapes as four points that criss-cross each other
         gc.fillPolygon(new double[]{10, 40, 10, 40},
                 new double[]{210, 210, 240, 240}, 4);
         gc.strokePolygon(new double[]{60, 90, 60, 90},
                 new double[]{210, 210, 240, 240}, 4);
+        // draw 'Z' shape manually as series of four points
         gc.strokePolyline(new double[]{110, 140, 110, 140},
                 new double[]{210, 210, 240, 240}, 4);
     }
