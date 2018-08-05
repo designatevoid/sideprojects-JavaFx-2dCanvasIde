05aa1a54fc7c727e492ad8ac28c8d53132c0b585
Michael Peter Gower Amiethyst <designatevoid@gmail.com>
2018-08-04 17:29:19 -0700

    broken-build: now has failing test.

---
+++ src/test/java/name/michaelamiethyst/AmiethystIdeTest.java
@@ -0,0 +1,23 @@
+package name.michaelamiethyst;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmiethystIdeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        AmiethystIde.main(null);
+    }
+}
